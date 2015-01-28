713a80c5c353df4f8a90e66a8a337bc73d55ae70
Macmini <macmini@MacminideMac-mini.local>
2015-01-28 23:18:21 +0800

    新工程

---
+++ Cosmetology/Cosmetology/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cosmetology
+//
+//  Created by Macmini on 15/1/28.
+//  Copyright (c) 2015年 hanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cosmetology/Cosmetology/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cosmetology
+//
+//  Created by Macmini on 15/1/28.
+//  Copyright (c) 2015年 hanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
